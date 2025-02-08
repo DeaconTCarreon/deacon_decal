c32b4f0423519be938f2fb869942a3e3ef211583
Deacon Carreon <deacontcarreon@gmail.com>
2025-02-08 15:22:43 -0800

    Finished hw2.py

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,29 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# cd ~/Desktop
+
 # 2) What does ~/ mean?
+# It helps you change directories to a specific folder of files, not a file itself.
+
 # 3) What's the difference between and abolute and a relative path in your terminal?
+# An absolute path is the direct path from your system origin to your current file, while a relative path is just the file's path from the cwd.
+
 # 4) What command would bring you back to your home directory?
-# 5) If you called rm ./ in your current working directory, what would it do? 
+# cd, which means "change directory," would exit out of all directories, bringing you back to your home one.
+
+# 5) If you called rm ./ in your current working directory, what would it do?
+# This is remove command, so it would remove any certain files or group of files from your current working directory.
+ 
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# "git add" adds a change in the working directory to the staging area, but you need to use "git commit" to actually record those changes in the repository. "git push" uploads the remote repository content to a local repository.
+
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# It should say "No commits yet" because you have only updated the directory, so it is telling you that you haven't done any commits that would record any changes to the repository.
+
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# The biggest error I had was trying to figure out how to add the SSH keys to my github account, but I was able to get them after redoing the process, knowing what to expect.
+
 # 9) How does cloning a repository differ from pulling from a repository?
+# Cloning a repository creates a copy of it that you can make your own edits and changes to, while pulling a repository just puts the original, un-editable version on your computer.
+
 # 10) Tell me a fun fact!
+# On the scale of a googol, the mass difference between an electron and the observable universe is basically non-existant!
